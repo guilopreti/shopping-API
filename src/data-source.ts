3ede5772bf50f0b0cd74fb18642bf7cb6b7d3907
Guilherme <guilherme.llopreti@gmail.com>
2022-05-13 15:55:04 -0300

    feature: configurações para o deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,13 +12,19 @@ export const AppDataSource =
       })
     : new DataSource({
         type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PWD,
-        database: process.env.POSTGRES_DB,
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_END === "production"
+            ? { rejectUnauthorized: false }
+            : false,
         synchronize: false,
         logging: true,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/entities/*.js"]
+            : ["src/entities/*.ts"],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/migrations/*.js"]
+            : ["src/migrations/*.ts"],
       });

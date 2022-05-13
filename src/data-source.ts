452a8321addd4def75eff4cfdb9bee542347a172
Guilherme <guilherme.llopreti@gmail.com>
2022-05-13 16:59:04 -0300

    fix: comando data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -14,7 +14,7 @@ export const AppDataSource =
         type: "postgres",
         url: process.env.DATABASE_URL,
         ssl:
-          process.env.NODE_END === "production"
+          process.env.NODE_ENV === "production"
             ? { rejectUnauthorized: false }
             : false,
         synchronize: false,
